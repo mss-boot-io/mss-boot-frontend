68cc98709eefc6e3d69721301243dac33c37d920
lwnmengjing <991154416@qq.com>
2022-03-17 15:56:50 +0800

    🐛 fix(config): 修复了一些 bug
    🐛 fix(config): 修复了一些 bug

---
+++ config/routes.ts
@@ -0,0 +1,73 @@
+﻿export default [
+  {
+    path: '/user',
+    layout: false,
+    routes: [
+      {
+        path: '/user',
+        routes: [
+          {
+            name: 'login',
+            path: '/user/login',
+            component: './user/Login',
+          },
+        ],
+      },
+      {
+        component: './404',
+      },
+    ],
+  },
+  {
+    path: '/welcome',
+    name: 'welcome',
+    icon: 'smile',
+    component: './Welcome',
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    icon: 'crown',
+    access: 'canAdmin',
+    component: './Admin',
+    routes: [
+      {
+        path: '/admin/sub-page',
+        name: 'sub-page',
+        icon: 'smile',
+        component: './Welcome',
+      },
+      {
+        component: './404',
+      },
+    ],
+  },
+  {
+    name: 'list.table-list',
+    icon: 'table',
+    path: '/list',
+    component: './TableList',
+  },
+  {
+    path: '/generator',
+    name: 'generator',
+    icon: 'crown',
+    access: 'canAdmin',
+    component: './generator/ServiceList',
+    routes: [
+      {
+        path: '/generator/service',
+        name: 'list.service',
+        icon: 'table',
+        component: './generator/ServiceList',
+      },
+    ],
+  },
+  {
+    path: '/',
+    redirect: '/welcome',
+  },
+  {
+    component: './404',
+  },
+];
