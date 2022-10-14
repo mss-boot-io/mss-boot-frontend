43e6f4646195646a00aa2e652ae86b1c1e98196e
lwnmengjing <lwnmengjing@qq.com>
2022-10-15 01:23:34 +0800

    Fix/origin (#7)

---
+++ config/config.prod.ts
@@ -0,0 +1,8 @@
+// https://umijs.org/config/
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  define: {
+    'process.env.BASE_URL': 'https://api.mss-boot-io.top',
+  },
+});
